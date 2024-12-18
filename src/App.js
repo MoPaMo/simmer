149f582a81e4ccdcfa80800c3ba546eab5bff695
MoPaMo <mopamo@users.noreply.github.com>
2024-12-18 10:53:04 +0100

    style: update App component styles and integrate Poppins font for improved aesthetics

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipe from "./components/AddRecipe";
-
+import "./App.css";
 function App() {
   return (
     <Router>
