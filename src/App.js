1dfaf65b72271de4c2562431c897a9e75420d15e
MoPaMo <mopamo@users.noreply.github.com>
2024-12-18 09:50:43 +0100

    refactor: update routing to use Routes and element prop in App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipe from "./components/AddRecipe";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>Simmer Recipe Manager</h1>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={RecipeList} />
-          <Route path="/add" component={AddRecipe} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipe />} />
+        </Routes>
       </div>
     </Router>
   );
