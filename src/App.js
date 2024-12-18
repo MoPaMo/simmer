42f2beed676539d84b4ae5d2b1579e9aed759c82
MoPaMo <mopamo@users.noreply.github.com>
2024-12-18 12:14:59 +0100

    feat: add route for RecipeDetail component to display individual recipe information

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipe from "./components/AddRecipe";
+import RecipeDetail from "./components/RecipeDetail";
 import NavBar from "./components/NavBar";
 import "./App.css";
+
 function App() {
   return (
     <Router>
@@ -17,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<RecipeList />} />
             <Route path="/add" element={<AddRecipe />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
           </Routes>
         </main>
       </div>
