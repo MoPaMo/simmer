d83572d07c89824267b51a8f4f01824528fe19af
MoPaMo <mopamo@users.noreply.github.com>
2024-12-18 12:16:52 +0100

    refactor: remove unnecessary comment from RecipeDetail component

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,4 +1,3 @@
-simmer / src / components / RecipeDetail.js;
 // src/components/RecipeDetail.js
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
